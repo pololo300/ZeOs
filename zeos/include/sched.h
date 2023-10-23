a051a48e3dfd39d4164683256dbb5bfd6b9a81c8
sharknet56 <129885883+sharknet56@users.noreply.github.com>
2023-10-23 09:03:57 +0200

    Update sched.h

--- zeos/include/sched.h
+++ zeos/include/sched.h
@@ -17,9 +17,8 @@ enum state_t { ST_RUN, ST_READY, ST_BLOCKED };
 struct task_struct {
   int PID;			/* Process ID. This MUST be the first field of the struct. */
   page_table_entry * dir_pages_baseAddr;
-  int ksp; 
+  unsigned long ksp; 
   struct list_head list;
-  unsigned long * ini_val_ebp;
 };
 
 union task_union {
